8ba91c6bc8fcb20247095fb8a29fde0c2a7d5021
I337509 <b.sultania@sap.com>
2019-06-08 14:48:50 +0530

    Codejudge

--- src/main/java/org/codejudge/sb/controller/TestController.java
+++ src/main/java/org/codejudge/sb/controller/TestController.java
@@ -3,6 +3,7 @@ package org.codejudge.sb.controller;
 import java.io.IOException;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,11 @@ public class TestController {
 	@ResponseBody 
 	public String test() throws IOException {
 		return "Hello Docker world!";
+	}
+
+    @GetMapping(value = "/api/leads/{lead_id}")
+	@ResponseBody
+	public String getLeadId(@PathVariable("lead_id") String lead_id) throws IOException{
+		return "This is working"+lead_id;
 	}
 }
